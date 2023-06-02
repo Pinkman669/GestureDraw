c260e14e3b21acb16ca247c696fad1a19b692d49
Naomi Chan <channaomi.b@gmail.com>
2023-06-02 12:04:45 -0400

    edit route

--- express_server/routers.ts
+++ express_server/routers.ts
@@ -3,16 +3,12 @@ import { rankingController } from "./main";
 
 
 
+
 export const rankingRoutes = () =>{
         const route = express.Router();
         route.get("/", rankingController.getRanking);
         return route;
 };
-
-export const gameRoutes = () =>{
-        const route = express.Router();
-        route.post("/",gameController.getusername)
-}
 
 
 
